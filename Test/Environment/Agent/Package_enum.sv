4d6fd31097c5b00b313dcebe5abf9985e794349a
sriniverse004 <nandansrinivaas2004@gmail.com>
2024-11-16 11:55:22 +0530

    Update and rename Libdef_enum.sv to Package_enum.sv

--- Test/Environment/Agent/Package_enum.sv
+++ Test/Environment/Agent/Package_enum.sv
@@ -3,7 +3,7 @@
 
 package ahb_enum_types;
   typedef enum bit {AHB_READ = 1'b0, AHB_WRITE = 1'b1} ahb_read_write_enum;
-  typedef enum bit[2:0] {AHB_DONE=3'b000, AHB_ERROR=3'b001, AHB_LAST=3'b010, AHB_RETRACT=3'b011, AHB_WAIT=3'b100} ahb_response_type_enum;
+  typedef enum bit[1:0] {AHB_DONE=2'b00, AHB_ERROR=2'b01, AHB_WAIT=2'b10, AHB_SPLIT=2'b11} ahb_response_type_enum;
 endpackage
 
 `endif
